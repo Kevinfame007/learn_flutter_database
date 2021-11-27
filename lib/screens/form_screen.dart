6d59b0fae9e15b1f73368901e1a57ec00772bc52
Kevinfame007 <kevinfame007@gmail.com>
2021-11-27 20:32:57 +0700

    learn how to create form interface
    learn how to create form interface and even onPressed

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -4,10 +4,35 @@ class FormScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("แบบฟอร์มบันทึกข้อมูล"),
-          
+      appBar: AppBar(
+        title: Text("แบบฟอร์มบันทึกข้อมูล"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Form(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                decoration: new InputDecoration(labelText: "ชื่อรายการ"),
+                autofocus: true,
+              ),
+              TextFormField(
+                decoration: new InputDecoration(labelText: "จำนวนเงิน"),
+                keyboardType: TextInputType.number,
+              ),
+              FlatButton(
+                child: Text("เพิ่มข้อมูล"),
+                color: Colors.purple,
+                textColor: Colors.white,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          ),
         ),
-        body: Container());
+      ),
+    );
   }
 }
