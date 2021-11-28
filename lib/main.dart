1493db39c519f4d74ea6162c819a78a1c66f8876
Kevinfame007 <kevinfame007@gmail.com>
2021-11-28 14:33:22 +0700

    learn how to use card widget
    learn how to use card widget

--- lib/main.dart
+++ lib/main.dart
@@ -32,19 +32,38 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          actions: [
-            IconButton(
-              icon: Icon(Icons.add),
-              onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return FormScreen();
-                }));
-              },
-            )
-          ],
-        ),
-        body: Container());
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return FormScreen();
+              }));
+            },
+          )
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: 4,
+        itemBuilder: (context, int index) {
+          return Card(
+            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+            elevation: 5,
+            child: ListTile(
+              leading: CircleAvatar(
+                radius: 30,
+                child: FittedBox(
+                  child: Text("500"),
+                ),
+              ),
+              title: Text("เมนู"),
+              subtitle: Text("28/11/2021"),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
