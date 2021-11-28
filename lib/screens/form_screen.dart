3be55073476d36f870673df4c042527754c3b4c1
Kevinfame007 <kevinfame007@gmail.com>
2021-11-28 21:38:01 +0700

    learn how to validation data
    learn how to validation data and check if input isEmpty system will nortify please enter input

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class FormScreen extends StatelessWidget {
+  final formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,23 +11,43 @@ class FormScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(10.0),
         child: Form(
+          key: formKey,
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               TextFormField(
                 decoration: new InputDecoration(labelText: "ชื่อรายการ"),
                 autofocus: true,
+                validator: (String? str) {
+                  //ชื่อรายาการเป็นค่าว่าง
+                  if(str!.isEmpty) {
+                    return "กรุณาป้อนชื่อรายการ";
+                  }
+                  return null;
+                },
               ),
               TextFormField(
                 decoration: new InputDecoration(labelText: "จำนวนเงิน"),
                 keyboardType: TextInputType.number,
+                validator: (String? str) {
+                  //ชื่อรายาการเป็นค่าว่าง
+                  if(str!.isEmpty) {
+                    return "กรุณาป้อนจำนวนเงิน";
+                  }
+                  if (double.parse(str)<=0) {
+                    return "กรุณาป้อนตัวเลขมากกว่า 0";
+                  }
+                  return null;
+                },
               ),
               FlatButton(
                 child: Text("เพิ่มข้อมูล"),
                 color: Colors.purple,
                 textColor: Colors.white,
                 onPressed: () {
-                  Navigator.pop(context);
+                  if(formKey.currentState!.validate()) {
+                    Navigator.pop(context);
+                  }
                 },
               )
             ],
